3cbdbecdda66bcb38090f89d48ac0550b0568f64
xamanDawud <ahmeddaudking100@gmail.com>
2022-08-13 23:39:57 +0600

    Html selecting system explore in dom

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 console.log("JavaScript is working with DOM");
 // console.log(document);
-console.log(document.body);
+// console.log(document.body);
+
+let item = document.getElementsByTagName("li");
+let pagriDada = document.getElementById("pagri_dada");
+let pagriItem = document.getElementsByClassName("pagri_item");
+for (const i of pagriItem) {
+    console.log(i.innerHTML);
+}
+
+// <----------------------Five way to we can access element in DOM ------------------------------>
+
+// First For accessing Html Tag
+document.getElementsByTagName("ul");
+
+// Second For accessing Id
+document.getElementById("pagri_dada");
+
+// Third For accessing Html Class
+document.getElementsByClassName("pagri_item");
+
+// Fourth For accessing any tag or class With querySelector
+document.querySelectorAll(".pagri_item"); // it will access the first element
+
+// Fifth For accessing first element in any tag with querySelectorAll
+document.querySelector("h1");
